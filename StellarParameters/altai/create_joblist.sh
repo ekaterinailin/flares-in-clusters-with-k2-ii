24e4b3e3d4379e95954580dc4cceed8f66319bc6
Ekaterina Ilin <eilin@aip.de>
2019-10-18 10:04:23 +0200

    some changes in the altaipony joblist and post-analysis notebook

--- StellarParameters/altai/create_joblist.sh
+++ StellarParameters/altai/create_joblist.sh
@@ -1,12 +1,12 @@
 
 OLDIFS=$IFS
 IFS=,
-while read ids channel campaign; do echo python /home/eilin/script_parallel.py $ids $channel $campaign ngc2168 >> joblist.sh ; done < ngc2168_altai.csv 
-while read ids channel campaign; do echo python /home/eilin/script_parallel.py $ids $channel $campaign ngc2682 >> joblist.sh ; done < ngc2682_altai.csv 
-while read ids channel campaign; do echo python /home/eilin/script_parallel.py $ids $channel $campaign pleiades >> joblist.sh ; done < pleiades_altai.csv 
-while read ids channel campaign; do echo python /home/eilin/script_parallel.py $ids $channel $campaign hyades >> joblist.sh ; done < hyades_altai.csv 
-while read ids channel campaign; do echo python /home/eilin/script_parallel.py $ids $channel $campaign praesepe >> joblist.sh ; done < praesepe_altai.csv 
-while read ids channel campaign; do echo python /home/eilin/script_parallel.py $ids $channel $campaign ngc6774 >> joblist.sh ; done < ngc6774_altai.csv 
+while read ids channel campaign; do echo python /home/eilin/FlaresInClusters/src/script_parallel.py $ids $channel $campaign ngc2168 >> joblist.sh ; done < ngc2168_altai.csv 
+while read ids channel campaign; do echo python /home/eilin/FlaresInClusters/src/script_parallel.py $ids $channel $campaign ngc2682 >> joblist.sh ; done < ngc2682_altai.csv 
+while read ids channel campaign; do echo python /home/eilin/FlaresInClusters/src/script_parallel.py $ids $channel $campaign pleiades >> joblist.sh ; done < pleiades_altai.csv 
+while read ids channel campaign; do echo python /home/eilin/FlaresInClusters/src/script_parallel.py $ids $channel $campaign hyades >> joblist.sh ; done < hyades_altai.csv 
+while read ids channel campaign; do echo python /home/eilin/FlaresInClusters/src/script_parallel.py $ids $channel $campaign praesepe >> joblist.sh ; done < praesepe_altai.csv 
+while read ids channel campaign; do echo python /home/eilin/FlaresInClusters/src/script_parallel.py $ids $channel $campaign ngc6774 >> joblist.sh ; done < ngc6774_altai.csv 
 IFS=$OLDIFS
 
 grep -v "EPIC" joblist.sh > temp && mv temp joblist.sh
